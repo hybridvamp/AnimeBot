60445f621e2f45eacb457ee6cf889828feaf4880
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2024-10-09 20:47:07 +0530

    get_messages

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ async def _start(event):
             # Iterate through the message IDs from the first to the last
             for msg_id in range(f_msg_id, l_msg_id + 1):
                 try:
-                    msg = await bot.get_message(f_chat_id, msg_id)
+                    msg = await bot.get_messages(f_chat_id, msg_id)
                     await msg.copy(message.chat_id, protect_content=(protect == "/pbatch"))
                 except FloodWaitError as e:
                     await asyncio.sleep(e.seconds + 1)
